99bf742e2ad86dcdcea2e5c2deb70edc61619aac
Yaungad Yasaway <yasaway@mail.ru>
2022-12-04 15:53:49 +0500

    создание ДЗ 1 урока

---
+++ task02/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите 2 числа");
+Console.Write("1 число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("2 число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine("Число " + a + " больше!");
+}
+else
+{
+    Console.WriteLine("Число " + b + " больше!");
+}
---
+++ task04/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44, 5, 78 -> 78
+// 22, 3, 9 -> 22
+
+Console.WriteLine("Введите 3 числа");
+Console.Write("1 число: ");
+int a1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("2 число: ");
+int a2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("3 число: ");
+int a3 = Convert.ToInt32(Console.ReadLine());
+if (a1 > a2 & a1 > a3)
+{
+    Console.WriteLine("Число " + a1 + " больше!");
+}
+else
+{
+    if (a2 > a1 & a2 > a3)
+    {
+        Console.WriteLine("Число " + a2 + " больше!");
+    }
+    else
+    {
+        Console.WriteLine("Число " + a3 + " больше!");
+    }
+}
